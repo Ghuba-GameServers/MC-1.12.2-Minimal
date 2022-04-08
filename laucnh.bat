a719aa94e92b619402df2e4b7fb85b14c07fd3c5
Anatoliy Tereshchenko <Anatoliy.Tereshchenko@udelphi.com>
2022-04-08 16:31:06 +0300

    Initial

---
+++ laucnh.bat
@@ -0,0 +1,3 @@
+echo "Starting server" 
+java -Xms1024M -Xmx2048M -jar forge-1.12.2-14.23.5.2859.jar nogui
+PAUSE
---
+++ laucnh.sh
@@ -0,0 +1,2 @@
+echo "Starting server" 
+java -Xms1024M -Xmx2048M -jar forge-1.12.2-14.23.5.2859.jar nogui
