c77cc6423fe697ed7481e0286587f1fe9a5c7fd6
Anatoliy Tereshchenko <Anatoliy.Tereshchenko@udelphi.com>
2022-04-11 17:24:49 +0300

    just save

--- laucnh.bat
+++ laucnh.bat
@@ -1,3 +1,3 @@
 echo "Starting server" 
-java -Xms1024M -Xmx2048M -jar forge-1.12.2-14.23.5.2859.jar nogui
+java -Xms1024M -Xmx2048M -jar forge-1.12.2-14.23.5.2860.jar nogui
 PAUSE
--- laucnh.sh
+++ laucnh.sh
@@ -1,2 +1,2 @@
 echo "Starting server" 
-java -Xms1024M -Xmx2048M -jar forge-1.12.2-14.23.5.2859.jar nogui
+java -Xms1024M -Xmx2048M -jar forge-1.12.2-14.23.5.2860.jar nogui
